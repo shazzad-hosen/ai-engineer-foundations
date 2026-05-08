891c3190402d3f3b9f6f72908b14ef54751e6f89
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-05-08 11:31:01 +0600

    starting with objects and classes

---
+++ 01. Python Fundamentals/14_object_orientation.py
@@ -0,0 +1,22 @@
+# class is a blueprint of an object
+class Student:
+    college = "ABC college of Engineering"
+    subject = "Python"
+    year = "3rd"
+    
+
+# objects, object is an instance of class
+student1 = Student()
+student2 = Student()
+
+print(student1)
+print(student2)
+
+print(student1.college)
+print(student2.college)
+
+print(student1.subject)
+print(student2.subject)
+
+print(student1.year)
+print(student2.year)
