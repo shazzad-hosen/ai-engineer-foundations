927415dde4e63cd83734599ac6ba6173c6c9ce29
Shazzad Hosen Zisan <mdshazzadhosenzisan@gmail.com>
2026-05-22 22:46:54 +0600

    class methods

--- 01. Python Fundamentals/17_methods.py
+++ 01. Python Fundamentals/17_methods.py
@@ -1,4 +1,3 @@
-# instance methods
 class Laptop:
     storage_type = "SSD"
     
@@ -6,13 +5,22 @@ class Laptop:
         self.ram = ram
         self.storage = storage
         
-    def get_info(self): # instance method -> can access instance attributes and class attributes 
+    # instance method -> can access instance attributes as well as class attributes
+    def get_info(self): 
         print(f"laptop has {self.ram} RAM & {self.storage} {self.storage_type}")
+        
+    # class method -> can only access class attributes
+    @classmethod # decorator function -> that makes the following method a class method
+    def get_storage_type(cls):
+        print(f"storage type is: {cls.storage_type}")
         
 laptop1 = Laptop("128GB", "4TB")
 laptop2 = Laptop("64GB", "1TB")
-laptop3 = Laptop("32GB", "512GB")
 
+# calling instance methods
 laptop1.get_info()
 laptop2.get_info()
-laptop3.get_info()
+
+# calling class methods
+laptop1.get_storage_type()
+Laptop.get_storage_type()
